d2646f3e3a97086115226d68326a45c8fe077e40
rohitvarma31094-spec <rohitvarma31094@gmail.com>
2026-08-31 21:25:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 import os
 
-import os
-
 API_ID = int(os.environ.get("API_ID", 12345678))
 API_HASH = os.environ.get("API_HASH", "your_api_hash_here")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "123456:ABC-your-bot-token")
