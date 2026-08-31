370c4dbe420a0f6a22a95f5726ee29056862f620
rohitvarma31094-spec <rohitvarma31094@gmail.com>
2026-08-31 18:05:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,8 +12,7 @@ ACCOUNTS_FILE = f"{DATA_DIR}/accounts.json"
 ADMINS_FILE = f"{DATA_DIR}/admins.json"
 SETTINGS_FILE = f"{DATA_DIR}/settings.json"
 CAMPAIGNS_FILE = f"{DATA_DIR}/campaigns.json"
-SCHEDULED_FILE = f"{DATA_DIR}/scheduled.json"  # ← YEH LINE ADD KARO
-
+SCHEDULED_FILE = f"{DATA_DIR}/scheduled.json"
 
 os.makedirs(DATA_DIR, exist_ok=True)
 os.makedirs(SESSIONS_DIR, exist_ok=True)
